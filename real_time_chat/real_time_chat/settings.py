97720bf1596d0ebe16a59eecf88bc65b412e8706
Jawad M <hafizjawad858@gmail.com>
2024-11-11 01:26:19 +0500

    adding search functionalites emojis and files

--- real_time_chat/real_time_chat/settings.py
+++ real_time_chat/real_time_chat/settings.py
@@ -12,23 +12,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1*=om_0o70axv04x@aw2vn(sj*@=72sy!(*9*^r^t-=zxd-*ql'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.100.84','*','192.168.0.107']
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'daphne',
